061162f44c525f5a3ebaed5cb94181161ede7fbf
Michael Douglas <michaellawrencedouglas@gmail.com>
2017-06-21 13:44:41 -0400

    try to access user data

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+
   function onPermission(){
     $('#fbPermission').on('click',function(event){
       event.preventDefault();
       FB.login(function(){}, {scope: 'user_friends'});
     });
 }
+function onTest(){
+  $('#btnTest').on('click',function(event){
+    FB.api('/me', {fields: 'last_name'}, function(response) {
+    console.log(response);
+    })
+  });
+}
 
 $(function(){
   onPermission();
 })
-alert('hey');
+// alert('hey');
