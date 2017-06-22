c52d3e36a8b68a42fa38b4e41ed7446f98b9b250
Michael Douglas <michaellawrencedouglas@gmail.com>
2017-06-22 11:14:39 -0400

    console .log api output

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function onPermission(){
       event.preventDefault();
       FB.login(function(){
         FB.api('/me', {fields: 'email,first_name'}, function(response) {
-          alert(response);
+          console.log(response);
         });
       }, {scope: 'user_friends,email,publish_actions'});
     });
