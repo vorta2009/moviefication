c58a9e1a3d67ebaaf8bc61f2f0d3e8ce927be2da
Michael Douglas <michaellawrencedouglas@gmail.com>
2017-06-29 19:36:36 -0400

    all systems functioning however some bugs still

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var app={
+
   OMDB:{
     url:'https://www.OMDBapi.com',
     apiKey:'e706bc8'
@@ -7,12 +8,11 @@ var app={
     url:'https://tastedive.com/api/similar',
     apiKey:'274055-moviefic-4YV5JJJB'
   },
-  movieArr:[],
-  firstMovie:{},
-  i:0,
-  endArr:false
+  primaryMovieName:'',
+  movieHTMLArr:[],
+  simObj:{},
+  i:-2
 };
-
 
 function makeTeaserText(wTeaser){
   var teaserLen=260;
@@ -28,39 +28,38 @@ function makeOMDBDiv(data){
   newDiv.find('#moviePic').attr('src',  (data.moviePic==="N/A"?'':data.moviePic)  );
   newDiv.find('#rating').text(data.rated);
   newDiv.find('#releaseYear').text(data.releaseYear);
-  newDiv.find('#plot').text(data.plot);
+  if (app.i>=0){
+    console.log("i: "+app.i + " movie: " +data.movieName);
+    newDiv.find('#plot').text(makeTeaserText(app.simObj.Results[app.i].wTeaser));
+    }
   newDiv.find('#metacritic').text(data.metacritic);
   newDiv.find('#IMDB').text(data.IMDB);
   newDiv.find('#rottenTomatoes').text(data.rottenTomatoes);
   newDiv.find('.getSimMovies').attr('mu-v',data.movieName);
+  newDiv.removeAttr('id');
+  app.movieHTMLArr.push(newDiv);
+  
+  if ('Results' in app.simObj)
+  {
+    if (app.i===app.simObj.Results.length-1){
+       $(app.movieHTMLArr[0]).find('#plot').text(makeTeaserText(app.simObj.Info[0].wTeaser));
+       app.movieHTMLArr.slice(1,app.movieHTMLArr.length).map(function(elem,index){
+        return $(elem).find('#plot').text(makeTeaserText(app.simObj.Results[index].wTeaser));
+       });
+  
 
-  if ($('.movieDiv').length===1){
-   $('#chosenMovie').append(newDiv);
-   // newDiv.find('#plot').text(data.movieName+": "+app.movieArr[app.i].wTeaser); 
+    app.movieHTMLArr=app.movieHTMLArr.slice(0,1).concat(app.movieHTMLArr.slice(1,app.movieHTMLArr.length).sort(function(a,b){
+      return parseFloat(  ($(b).find('#metacritic').text()==="N/A" || $(b).find('#rating').text()==="R")?"0":$(b).find('#metacritic').text())
+        - parseFloat(($(a).find('#metacritic').text()==="N/A" || $(a).find('#rating').text()==="R")?"0":$(a).find('#metacritic').text())
+      }));
+    $('#moviesHere').append(app.movieHTMLArr);
+}
   }
-  else{
-    newDiv.attr('divNum',app.i);
-    newDiv.find('#plot').text(makeTeaserText(app.movieArr[app.i].wTeaser));
-    $('#moviesHere').append(newDiv);
-    app.i++;
-  }
-  if (app.endArr){
-    console.log(app.i);
-    console.log('in appEndArr');
-    $('#moviesHere').find('.movieDiv').sort(function(a,b){
-      return parseFloat($(b).find('#metacritic').text()==="N/A"?"0":$(b).find('#metacritic').text())
-        -parseFloat($(a).find('#metacritic').text()==="N/A"?"0":$(a).find('#metacritic').text());
-    }).appendTo('#moviesHere');
-    $('#chosenMovie').find('.movieDiv').removeAttr('id');
-    $('#moviesHere').find('.movieDiv').removeAttr('id');
-    $('#chosenMovie #plot').text(makeTeaserText(app.firstMovie[0].wTeaser));
-  } 
 }
 
 function runReviewAJAX(data){
 // Callback function for OMDB API, collects all relevant movie info and passes as object to >>> makeOMDBDiv  
-  
-
+    app.i++;
     if('Error' in data){return;}
   var OMDBData={
     movieName:data.Title,
@@ -72,29 +71,20 @@ function runReviewAJAX(data){
     plot:data.Plot,
     moviePic:data.Poster,
   };
-  app.endArr= (app.i===app.movieArr.length-1);
+  // console.log(OMDBData);
   makeOMDBDiv(OMDBData);
 }
 
-function onMovieReviews(){
-  // Handles user querying a movie in top-left movie bar
-  $('#movieReviews').on('submit',function(event){
-    event.preventDefault();
-    var movieName=$(this).find('input').val();
-    createPage(movieName);
-  });
-}
 
 function handleTasteDiveOutput(data){
   //store an array of tasteDive similar movie results in our app variable to access later
   console.log(data.Similar.Results);
-  app.movieArr=data.Similar.Results;
+  app.movieHTMLArr=data.Similar.Results;
   app.firstMovie=data.Similar.Info;
-  for (i=0;i<app.movieArr.length;i++){
-    findReviews(app.movieArr[i].Name);
-    // console.log(i + "/" + app.movieArr.length);
-    // console.log(app.endArr);
+  for (i=0;i<app.movieHTMLArr.length;i++){
+    findReviews(app.movieHTMLArr[i].Name);
   }
+
 }
 
 function findSimMovies(movieName){
@@ -105,8 +95,15 @@ function findSimMovies(movieName){
         verbose:"1",
         limit:"20"
     };
-    // console.log(settings);
-    $.getJSON(app.tasteDive.url,settings,handleTasteDiveOutput);
+    $.getJSON(app.tasteDive.url,settings,function(result){   
+        app.simObj=result.Similar;
+     })
+    .done(function(){
+        for(i=0;i<app.simObj.Results.length;i++){
+          findReviews(app.simObj.Results[i].Name);
+        }
+    });
+
 }
 
 function findReviews(movieName){
@@ -115,21 +112,37 @@ function findReviews(movieName){
       t: movieName
     };
     console.log(movieName);
-    $.getJSON(app.OMDB.url,OMDBSettings,runReviewAJAX);
-
+    $.getJSON(app.OMDB.url,OMDBSettings, function(result){
+        runReviewAJAX(result);
+        });
 }
+function findReviews1(movieName){
+  // This is supposed to find the correct movie name for the top-line movie
+      var OMDBSettings={           
+      apikey: app.OMDB.apiKey,
+      t: movieName
+    };
+    console.log(movieName);
+    $.getJSON(app.OMDB.url,OMDBSettings).done( function(result){
+        app.primaryMovieName=result.Title;
+        runReviewAJAX(result);
+      });
+}
+
+
 
 function createPage(movieName){
  // 
 $('#chosenMovie').empty();
 $('#moviesHere').empty();
-app.movieArr=[];
-app.endArr=false;
-app.i=0;
-app.firstMovie={};
+app.movieHTMLArr=[];
+app.simObj={};
+app.primaryMovieName="";
+app.i=-2;
+
  findReviews(movieName); 
   findSimMovies(movieName);
-
+  
 }
 
 function onSimilarMovies(){
@@ -139,6 +152,14 @@ function onSimilarMovies(){
   });
 }
 
+function onMovieReviews(){
+  // Handles user querying a movie in top-left movie bar
+  $('#movieReviews').on('submit',function(event){
+    event.preventDefault();
+    var movieName=$(this).find('input').val();
+    createPage(movieName);
+  });
+}
 
 $(function(){
   onMovieReviews();
